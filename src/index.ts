ef3d63b00994cfe959ff11ba1f8e8ca3f96ed4b5
Gary <fishellgary@gmail.com>
2023-06-01 01:56:13 -0700

    Added class diagram for tables. Finished setting up prisma. Started swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response} from "express";
 import bodyParser from "body-parser";
+import cors from 'cors'
 
-const app: Application = express()
+export const app: Application = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "<http://localhost:3000>" }))
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Healthy")
@@ -17,3 +19,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 
 })
+
