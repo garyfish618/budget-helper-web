dffce145e7a4b76495f9f48ff7ec1ed586e3015b
Gary <fishellgary@gmail.com>
2023-06-16 23:10:11 -0700

    Fixed cors for frontend

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const swaggerDocument = YAML.load("./swagger.yaml")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: "<http://localhost:3000>" }))
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(routes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
